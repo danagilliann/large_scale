b7d6bbde99937cbf8a662769eaaf6aff240f1711
Laura <laurachevalier4@yahoo.com>
2017-12-01 18:58:26 -0500

    adding necessary imports

--- web/scalica/micro/models.py
+++ web/scalica/micro/models.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.db import models
 from django.forms import ModelForm, TextInput
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
