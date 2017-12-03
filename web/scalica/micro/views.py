a5f7c3bb15177dc2fbd29de072bf9bf4d4fa520a
Betty Bei <betty.bei@nyu.edu>
2017-12-03 10:33:48 -0800

    cleanup

--- web/scalica/micro/views.py
+++ web/scalica/micro/views.py
@@ -1,4 +1,3 @@
-from django import forms
 from django.contrib.auth import logout, login, authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
